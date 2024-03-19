2a930012bde5af147abad5c36aa4059d9b827aa6
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-03-19 08:54:06 +0530

    added component in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Accordion from "./components/accordion/Accordion";
+import RandomColor from "./components/randomColor/RandomColor";
 
 function App() {
   return (
     <>
       <Accordion />
+      <RandomColor />
     </>
   );
 }
