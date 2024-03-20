3fd3693c5619900af3e83190da91344405c2643c
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-03-21 03:50:39 +0530

    added url and loading

--- src/components/imageSlider/ImageSlider.jsx
+++ src/components/imageSlider/ImageSlider.jsx
@@ -3,7 +3,7 @@
 import { useEffect } from "react";
 import { useState } from "react";
 
-export default async function ImageSlider({ url, limit }) {
+export default async function ImageSlider({ url, limit = 5, page = 1 }) {
   const [currSlide, setCurrSlide] = useState(0);
   const [image, setImage] = useState([]);
   const [loading, setLoading] = useState(false);
@@ -11,12 +11,11 @@ export default async function ImageSlider({ url, limit }) {
   async function fetchImages(url) {
     try {
       setLoading(true);
-      const response = await fetch("" + url + limit);
+      const response = await fetch(`${url}?page=${page}&limit=${limit}`);
       const data = await response.json();
       if (data) {
         setImage(data);
         setLoading(false);
-        console.log(data);
       }
     } catch (e) {
       setLoading(false);
@@ -32,8 +31,12 @@ export default async function ImageSlider({ url, limit }) {
 
   return (
     <div className="container">
-      <div>{loading ? "Loading..." : null}</div>
-      {/* <div>{(url, limit)}</div> */}
+      {setLoading(true)}
+      {loading ? (
+        <div className="flex justify-center content-center">{"Loading..."}</div>
+      ) : (
+        <div></div>
+      )}
     </div>
   );
 }
