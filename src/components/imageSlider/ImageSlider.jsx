569d36642af997b77f00d1351ccb425f6ce131db
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-03-28 09:56:48 +0530

    added useState for setting image data

--- src/components/imageSlider/ImageSlider.jsx
+++ src/components/imageSlider/ImageSlider.jsx
@@ -1,17 +1,18 @@
 import { useState } from "react";
 import { useEffect } from "react";
 import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from "react-icons/fa";
+
 export default function ImageSlider() {
   const [currentSlide, setCurrentSlide] = useState(0);
-  let length = 0;
+  const [images, setImages] = useState([]);
+  let length = images.length;
   const url = `https://picsum.photos/v2/list?page=2&limit=10`;
 
   async function getImages(getUrl) {
     const response = await fetch(getUrl);
     const data = response.json();
-    length = data.length;
+    setImages(data);
   }
-
   const nextSlider = () => {
     setCurrentSlide(currentSlide === length - 1 ? 0 : currentSlide + 1);
   };
@@ -19,9 +20,17 @@ export default function ImageSlider() {
   const prevSlider = () => {
     setCurrentSlide(currentSlide === 0 ? length - 1 : currentSlide - 1);
   };
+
   useEffect(() => {
     getImages(url);
   }, [url]);
 
-  return <div className="container"></div>;
+  return (
+    <div className="container">
+      \
+      <FaArrowAltCircleLeft className="left-arrow" onClick={prevSlider} />
+      <FaArrowAltCircleRight className="right-arrow" onClick={nextSlider} />
+      {images.map((image, index) => {})}
+    </div>
+  );
 }
