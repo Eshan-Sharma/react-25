fd8feb5957197ded57bb99ee79c9392bf6561b99
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-03-29 22:39:45 +0530

    added load more button

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import StarRating from "./components/starRating/StarRating";
 import Accordion from "./components/accordion/Accordion";
 import RandomColor from "./components/randomColor/RandomColor";
 import ImageSlider from "./components/imageSlider/ImageSlider";
+import LoadMoreButton from "./components/loadMoreButton/LoadMoreButton";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <RandomColor />
       <StarRating noOfStars={10} />
       <ImageSlider />
+      <LoadMoreButton />
     </>
   );
 }
