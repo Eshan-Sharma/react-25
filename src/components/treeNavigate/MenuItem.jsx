afef135018394c0531545034efd987c00b40a10c
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-03-30 11:15:00 +0530

    completed the toggle functionability

--- src/components/treeNavigate/MenuItem.jsx
+++ src/components/treeNavigate/MenuItem.jsx
@@ -1,12 +1,29 @@
+import { useState } from "react";
 import MenuList from "./MenuList";
 
 /* eslint-disable react/prop-types */
 export default function MenuItem({ item }) {
+  const [displayCurrentChildren, setDisplayCurrentChildren] = useState({});
+  function handleToggle(getCurrentLevel) {
+    setDisplayCurrentChildren({
+      ...displayCurrentChildren,
+      [getCurrentLevel]: !displayCurrentChildren[getCurrentLevel],
+    });
+  }
   return (
-    <li className="menu-item-container">
-      <p>{item.label}</p>
-
-      {item && item.children && item.children.length > 0 ? (
+    <li>
+      <div>
+        <p>{item.label}</p>
+        {item && item.children && item.children.length ? (
+          <span onClick={() => handleToggle(item.label)}>
+            {displayCurrentChildren[item.label] ? "-" : "+"}
+          </span>
+        ) : null}
+      </div>
+      {item &&
+      item.children &&
+      item.children.length > 0 &&
+      displayCurrentChildren[item.label] == true ? (
         <MenuList list={item.children} />
       ) : null}
     </li>
