3c0f981bc8a176bd46715b3cb75c6a29c746aafe
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-03-30 13:27:19 +0530

    Revert "debugged tailwind"
    This reverts commit 31acb922adad944250ae3748fe608fe7567f3b09.
    
    	modified:   src/components/themeChange/ThemeChange.jsx

--- src/components/themeChange/ThemeChange.jsx
+++ src/components/themeChange/ThemeChange.jsx
@@ -6,22 +6,12 @@ export default function ThemeChange() {
     setTheme(theme === "dark" ? "light" : "dark");
   }
   return (
-    <div
-      className={`light-dark-mode ${
-        theme === "light" ? "bg-white dark:bg-black" : "bg-black dark:bg-white"
-      } min-h-screen flex flex-col justify-center items-center text-2xl transition-all duration-500`}
-    >
+    <div className="light-dark-mode bg-white dark:bg-black min-h-screen flex flex-col justify-center items-center text-2xl transition-all duration-500">
       <div className="container flex flex-col justify-center items-center gap-6">
-        <p
-          className={`${theme === "light" ? "text-black" : "text-white"} ${
-            theme === "light" ? "dark:text-white" : "dark:text-black"
-          } text-5xl`}
-        >
-          Hello, world!
-        </p>
+        <p className="text-black dark:text-white text-5xl">Hello, world!</p>
         <button
           onClick={handleToggleTheme}
-          className={`bg-black dark:bg-white border border-black dark:border-white text-white dark:text-black px-4 py-2 rounded-lg font-semibold transition-all duration-300`}
+          className="bg-black dark:bg-white border border-black dark:border-white text-white dark:text-black px-4 py-2 rounded-lg font-semibold transition-all duration-300"
         >
           Toggle Mode
         </button>
