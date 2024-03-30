8f514ae5dd4a3b9cafd9e041a5cacf65704677a8
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-03-30 12:21:02 +0530

    added tailwind styling to button

--- src/components/qrCodeGenerator/QrCodeGenerator.jsx
+++ src/components/qrCodeGenerator/QrCodeGenerator.jsx
@@ -10,7 +10,7 @@ export default function QrCodeGenerator() {
     setInput("");
   }
   return (
-    <div>
+    <div className="flex flex-col items-center">
       <h1>QR code generator</h1>
       <div>
         <input
@@ -19,14 +19,25 @@ export default function QrCodeGenerator() {
           name="qr-code"
           placeholder="enter value"
         ></input>
-        <button
-          disabled={input && input.trim !== "" ? false : true}
-          onClick={() => {
-            handleQrCodeGenerate;
-          }}
-        >
-          Generate
-        </button>
+        {input && input.trim !== "" ? (
+          <button
+            type="button"
+            onClick={() => {
+              handleQrCodeGenerate;
+            }}
+            className="ml-2 pl-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+          >
+            Generate
+          </button>
+        ) : (
+          <button
+            type="button"
+            disabled={true}
+            className="ml-2 pl-2 py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+          >
+            Generate
+          </button>
+        )}
       </div>
       <div>
         <QRCode id="qr-code-value" value={qrCode} className="bg-white mb-10" />
