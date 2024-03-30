0f0971cd4dd669809279fce4dc698510dcf27b94
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-03-30 11:50:28 +0530

    added Tree Navigate Component

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ import Accordion from "./components/accordion/Accordion";
 import RandomColor from "./components/randomColor/RandomColor";
 import ImageSlider from "./components/imageSlider/ImageSlider";
 import LoadMoreButton from "./components/loadMoreButton/LoadMoreButton";
-
+import TreeNavigate from "./components/treeNavigate/TreeNavigate";
+import menus from "./components/treeNavigate/data";
 function App() {
   return (
     <>
@@ -13,6 +14,7 @@ function App() {
       <StarRating noOfStars={10} />
       <ImageSlider />
       <LoadMoreButton />
+      <TreeNavigate menus={menus} />
     </>
   );
 }
