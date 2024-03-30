0501fe272a1e687d10827ba8f329806cef0d27fe
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-03-30 10:12:09 +0530

    added menu list and menu item logic

--- src/components/treeNavigate/MenuItem.jsx
+++ src/components/treeNavigate/MenuItem.jsx
@@ -1,1 +1,4 @@
-export default function MenuItem() {}
+/* eslint-disable react/prop-types */
+export default function MenuItem({ item }) {
+  return <div className="menu-item-container">{item}</div>;
+}
--- src/components/treeNavigate/MenuList.jsx
+++ src/components/treeNavigate/MenuList.jsx
@@ -1,1 +1,12 @@
-export default function MenuList() {}
+/* eslint-disable react/prop-types */
+import MenuItem from "./MenuItem";
+
+export default function MenuList({ list = [] }) {
+  return (
+    <div className="menu-list-container">
+      {list && list.length
+        ? list.map((item, index) => <MenuItem key={index} item={item} />)
+        : null}
+    </div>
+  );
+}
