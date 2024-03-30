f3d586e5b3d760cb122bcd17070678ae61eb0584
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-03-30 12:25:20 +0530

    debugged onclick and clear input field

--- src/components/qrCodeGenerator/QrCodeGenerator.jsx
+++ src/components/qrCodeGenerator/QrCodeGenerator.jsx
@@ -5,7 +5,6 @@ export default function QrCodeGenerator() {
   const [qrCode, setQrCode] = useState("");
   const [input, setInput] = useState("");
   function handleQrCodeGenerate() {
-    console.log(input);
     setQrCode(input);
     setInput("");
   }
@@ -17,13 +16,14 @@ export default function QrCodeGenerator() {
           onChange={(e) => setInput(e.target.value)}
           type="text"
           name="qr-code"
+          value={input}
           placeholder="enter value"
         ></input>
         {input && input.trim !== "" ? (
           <button
             type="button"
             onClick={() => {
-              handleQrCodeGenerate;
+              handleQrCodeGenerate();
             }}
             className="ml-2 pl-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
           >
