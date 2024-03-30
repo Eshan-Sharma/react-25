28c64ba1552105cf0c8f7b68850c21de0462f57f
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-03-30 10:37:39 +0530

    added ul to list

--- src/components/treeNavigate/MenuList.jsx
+++ src/components/treeNavigate/MenuList.jsx
@@ -3,10 +3,10 @@ import MenuItem from "./MenuItem";
 
 export default function MenuList({ list = [] }) {
   return (
-    <div className="menu-list-container">
+    <ul className="menu-list-container">
       {list && list.length
         ? list.map((item, index) => <MenuItem key={index} item={item} />)
         : null}
-    </div>
+    </ul>
   );
 }
