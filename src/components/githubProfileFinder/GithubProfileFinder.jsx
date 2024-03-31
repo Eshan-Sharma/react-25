b9dc2bda1db2d5c6f9cd051b2e4150af876aaa30
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-04-01 02:47:42 +0530

    added tailwind styling

--- src/components/githubProfileFinder/GithubProfileFinder.jsx
+++ src/components/githubProfileFinder/GithubProfileFinder.jsx
@@ -32,15 +32,21 @@ export default function GithubProfileFinder() {
   }, []);
 
   return (
-    <div className="container">
-      <div className="input-wrapper">
+    <div className="container mx-auto w-90 max-w-1000 p-5 rounded-lg bg-white">
+      <div className="input-wrapper flex gap-4 justify-center mb-5">
         <input
           onChange={(e) => setUsername(e.target.value)}
           name="search-by-username"
           type="text"
+          className="px-4 py-2 text-lg border rounded"
           placeholder="Enter Github Username"
         ></input>
-        <button onClick={handleSearch}>Search</button>
+        <button
+          className="px-4 py-1 rounded bg-blue-400 text-black cursor-pointer"
+          onClick={handleSearch}
+        >
+          Search
+        </button>
       </div>
       {loading ? (
         <div>Loding please wait!</div>
