341f1747ddf6fb4054e9373d04a7d6591f56304a
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-04-01 02:47:51 +0530

    added tailwind styling

--- src/components/githubProfileFinder/UserCard.jsx
+++ src/components/githubProfileFinder/UserCard.jsx
@@ -3,25 +3,32 @@ export default function UserCard({ user }) {
   const { login, avatar_url, following, followers, public_repos, html_url } =
     user;
   return (
-    <div className="user">
-      <div className="avatar">
-        <img src={avatar_url} />
-      </div>
-      <div className="name">
-        <a href={html_url}>{login}</a>
-      </div>
-      <div className="profile">
-        <div>
-          <p>Public Repos</p>
-          <p>{public_repos}</p>
+    <div className="user p-4 border rounded">
+      <div className="flex items-center gap-4">
+        <div className="avatar h-12 w-12 rounded-full">
+          <img src={avatar_url} />
         </div>
-        <div>
-          <p>Followers</p>
-          <p>{followers}</p>
+        <div className="name flex gap-4 justify-center  align-center mt-5">
+          <a className="text-2xl font-bold" href={html_url}>
+            {login}
+          </a>
         </div>
-        <div>
-          <p>Following</p>
-          <p>{following}</p>
+      </div>
+
+      <div className="profile  flex justify-center mt-5">
+        <div className="flex gap-4">
+          <div>
+            <p>Public Repos</p>
+            <p>{public_repos}</p>
+          </div>
+          <div>
+            <p>Followers</p>
+            <p>{followers}</p>
+          </div>
+          <div>
+            <p>Following</p>
+            <p>{following}</p>
+          </div>
         </div>
       </div>
     </div>
