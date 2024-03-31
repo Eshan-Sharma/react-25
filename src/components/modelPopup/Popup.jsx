a856d20c6b4765c4cdd11f88cba15b3571052e41
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-04-01 01:01:18 +0530

    made popup component

--- src/components/modelPopup/Popup.jsx
+++ src/components/modelPopup/Popup.jsx
@@ -1,3 +1,12 @@
-export default function Popup() {
-  return <div>Ram Ram</div>;
+export default function Popup({ id, header, body, footer }) {
+  return (
+    <div id={id || "Model"} className="model">
+      <div className="header">
+        <span className="close-model-icon">&times;</span>
+        <header>{header ? header : "Header"}</header>
+      </div>
+      <div className="body">{body ? body : <div>Body</div>}</div>
+      <div className="footer">{footer ? footer : <h2>Footer</h2>}</div>
+    </div>
+  );
 }
