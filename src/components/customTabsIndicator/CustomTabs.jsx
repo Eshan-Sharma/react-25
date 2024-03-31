18fdb182e311ffa621f12da7bf239c33b9de2740
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-03-31 10:42:39 +0530

    added tab click content change functionality

--- src/components/customTabsIndicator/CustomTabs.jsx
+++ src/components/customTabsIndicator/CustomTabs.jsx
@@ -1,10 +1,22 @@
+import { useState } from "react";
+
 /* eslint-disable react/prop-types */
-export default function CustomTabs({ tabsContent }) {
+export default function CustomTabs({ tabsContent, onChange }) {
+  const [currentTabIndex, setCurrentTabIndex] = useState(0);
+  function handleOnclick(getIndex) {
+    setCurrentTabIndex(getIndex);
+    onChange(getIndex);
+  }
   return (
     <div>
-      {tabsContent.map((tabItem) => (
-        <div key={tabItem.label}>{tabItem.label}</div>
+      {tabsContent.map((tabItem, index) => (
+        <div onClick={() => handleOnclick(index)} key={tabItem.label}>
+          {tabItem.label}
+        </div>
       ))}
+      <div className="content">
+        {tabsContent[currentTabIndex] && tabsContent[currentTabIndex].content}
+      </div>
     </div>
   );
 }
