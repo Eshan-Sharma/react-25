3561957fd31e457c0329bdc31dae207a8d6d35c4
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-03-31 10:49:06 +0530

    added tailwind styling

--- src/components/customTabsIndicator/CustomTabs.jsx
+++ src/components/customTabsIndicator/CustomTabs.jsx
@@ -8,13 +8,24 @@ export default function CustomTabs({ tabsContent, onChange }) {
     onChange(getIndex);
   }
   return (
-    <div>
-      {tabsContent.map((tabItem, index) => (
-        <div onClick={() => handleOnclick(index)} key={tabItem.label}>
-          {tabItem.label}
-        </div>
-      ))}
-      <div className="content">
+    <div className="wrapper p-2">
+      <div className="heading mb-3 flex justify-center">
+        {tabsContent.map((tabItem, index) => (
+          <div
+            className={`tab-item ${
+              currentTabIndex === index
+                ? "bg-green-600 text-white text-lg font-bold px-4 py-2 cursor-pointer"
+                : "bg-purple-900 text-white text-lg font-bold px-4 py-2 mr-2 cursor-pointer"
+            }`}
+            onClick={() => handleOnclick(index)}
+            key={tabItem.label}
+          >
+            <span className="label">{tabItem.label}</span>
+          </div>
+        ))}
+      </div>
+
+      <div className="content p-4 overflow-auto bg-white">
         {tabsContent[currentTabIndex] && tabsContent[currentTabIndex].content}
       </div>
     </div>
