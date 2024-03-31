fa0a01791fae65495be3255cc7f6204cd9429595
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-04-01 01:45:46 +0530

    fetching data from github api

--- src/components/githubProfileFinder/GithubProfileFinder.jsx
+++ src/components/githubProfileFinder/GithubProfileFinder.jsx
@@ -1,3 +1,37 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
 export default function GithubProfileFinder() {
-  return <div></div>;
+  const [username, setUsername] = useState("eshan-sharma");
+  const [userData, setUserData] = useState([]);
+
+  function handleSearch() {
+    console.log(username);
+  }
+
+  async function fetchGithubUserData() {
+    const response = await fetch(`https://api.github.com/users/${username}`);
+    const userData = await response.json();
+    if (userData) {
+      setUserData(userData);
+      console.log(userData);
+    }
+  }
+  useEffect(() => {
+    fetchGithubUserData();
+  }, []);
+
+  return (
+    <div className="container">
+      <div className="input-wrapper">
+        <input
+          onChange={(e) => setUsername(e.target.value)}
+          name="search-by-username"
+          type="text"
+          placeholder="Enter Github Username"
+        ></input>
+        <button onClick={handleSearch}>Search</button>
+      </div>
+    </div>
+  );
 }
