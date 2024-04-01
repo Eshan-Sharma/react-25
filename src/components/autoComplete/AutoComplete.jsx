a89b8844154dd547c23ca9fd4fa94e17eab83be6
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-04-01 21:08:08 +0530

    fetching user dtaa

--- src/components/autoComplete/AutoComplete.jsx
+++ src/components/autoComplete/AutoComplete.jsx
@@ -1,3 +1,42 @@
+import { useEffect, useState } from "react";
+
 export default function AutoComplete() {
-  return <div>AutoComplete</div>;
+  const [userData, setUserData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [userInput, setUserInput] = useState("");
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  async function fetchUserData() {
+    try {
+      const response = await fetch("https://dummyjson.com/users");
+      const data = await response.json();
+      if (data && data.users && data.users.length > 0) {
+        setUserData(data.users.map((userItem) => userItem.firstName));
+        // setUserData(data);
+        setLoading(false);
+      }
+    } catch (error) {
+      console.error(error);
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    setLoading(true);
+    fetchUserData();
+  }, []);
+
+  return (
+    <div className="container">
+      <input
+        className="search-users"
+        placeholder="Find user"
+        onChange={(e) => {
+          setUserInput(e.target.value);
+        }}
+      ></input>
+      {console.log(userData)}
+      {loading ? <div>Loading please wait!</div> : null}
+    </div>
+  );
 }
